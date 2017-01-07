56732afb04ab0681742e053c9089593b144dcff9
rajan <rajan>
2017-01-07 20:44:58 +0530

    demo for home

---
+++ application/views/public/layout/footer.php
@@ -0,0 +1,10 @@
+
+        <footer>
+
+        </footer>
+		
+		<script src="<?php echo base_url($frameworks_dir . '/jquery/jquery.min.js'); ?>"></script>
+        <script src="<?php echo base_url($frameworks_dir . '/bootstrap/js/bootstrap.min.js'); ?>"></script>
+		
+    </body>
+</html>

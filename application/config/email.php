c3467f74efe0b4e48a3c28fbfa6bbdd5baf7d5f5
rajan <rajan>
2017-02-12 11:33:33 +0530

    send grid

---
+++ application/config/email.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * Config for the Plivo library
+ */
+$config['protocol'] = 'smtp';
+
+$config['smtp_port'] = '587';
+
+$config['smtp_host'] = 'smtp.sendgrid.net';
+
+$config['smtp_user'] = 'sanjeev.singh82@gmail.com';
+
+$config['smtp_pass'] = 'smidi001';

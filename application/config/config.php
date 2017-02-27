c99682e7ecb3bc2e5369ee453e21828535aee8f8
Gautam Lohar <gautamlohar@gmail.com>
2017-02-27 22:33:46 +0530

    KKK

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://ec2-35-154-95-127.ap-south-1.compute.amazonaws.com/';
+
+$config['base_url'] = 'http://localhost/mentorcell/';
 
 /*
 |--------------------------------------------------------------------------

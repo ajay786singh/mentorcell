09c1f1cfd7ba9e5586fbc3633ac524cde002fb16
mishrazs <ashutosh@zombiesoftwares.com>
2017-03-02 15:20:04 +0530

    static pages

---
+++ application/views/public/static/testimonial.php
@@ -0,0 +1,349 @@
+<section class="bgWhite">
+<div class="container">
+<div class="row">
+<div class="containerBox">
+<div class="sectionHeading">Our Management Team</div>
+
+<div class="teamRow">
+<div class="teamPic"><img src="images/ak-gupta.jpg"></div>
+<div class="teamDetail">
+<h3>Amit Kumar Gupta</h3>
+<h4>Co-Founder</h4>
+<p>Amit Kumar Gupta did his Bachelors and Masters in Mechanical Engineering from IIT Kharagpur and holds a laurea Magistrale in Industrial Engineering & Management from University of Udine, Italy. He has more than ten years of experience in Manufacturing Industry in the field of Supply Chain, in India and Italy. During his job tenure, he has been continuously guiding, training and developing teams working under him. He has a deep interest in Mentoring and high belief in moral education.</p>
+</div>
+</div>
+
+<div class="teamRow">
+<div class="teamPic"><img src="images/sanjeev-singh.jpg" /></div>
+<div class="teamDetail">
+<h3>Sanjeev Singh</h3>
+<h4>Co-Founder</h4>
+<p>Sanjeev Singh holds a Masters in Computer Science from USC, Columbia, USA and Bachelors & Masters in Maths and Computing from IIT Kharagpur. He has over 10 years of experience in industry and academia. He has worked as a web and app developer, and was involved in research in mobile computing, particularly in smartphone assisted safe driving.</p>
+</div>
+</div>
+
+<div class="teamRow">
+<div class="teamPic"><img src="images/p-chaudhuri.jpg" /></div>
+<div class="teamDetail">
+<h3>Pranab Chaudhuri</h3>
+<h4>Co-Founder</h4>
+<p>Pranab Chaudhuri holds a Bachelors and Masters in Maths & Computing from IIT Kharagpur. He has around 11 years of experience in industry working with High Tech companies like Oracle and VMware. He underwent training in 3D immersive visualization at NASA John C Stennis Space Center, Mississippi. He has worked in various domains like cloud, SAAS, Analystics etc.</p>
+</div>
+</div>
+
+<div class="teamRow">
+<div class="teamPic"><img src="images/d-mishra.jpg" /></div>
+<div class="teamDetail">
+<h3>Dinesh Mishra</h3>
+<h4>Co-Founder</h4>
+<p>Dinesh Mishra did his Graduation in Hotel Management and MBA in Hospitality Management. He has almost 13 years of experience in Hospitality industry in India and abroad. He also has more than ten years of experience in educational consultancy sector and founded Edu Mantra Educational Society in the year 2009.</p>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</section>
+
+
+
+
+
+
+
+<footer class="footerSection">
+<div class="container">
+<section class="footerTop">
+
+<div class="footerWidget">
+<h3>Website Name</h3>
+<p>G-130, Sec-63,<br>
+Noida, UP<br>
+201301</p>
+<p>info@mentorcell.com</p>
+</div>
+
+<div class="footerWidget">
+<h3>About us</h3>
+<ul>
+<li><a href="company-overview.html">Company Overview</a></li>
+<li><a href="#">What we do</a></li>
+<li><a href="#">Testimonial</a></li>
+<li><a href="team.html">Team</a></li>
+</ul>
+</div>
+
+<div class="footerWidget">
+<h3>Services</h3>
+<ul>
+<li><a href="#">Admission 2017</a></li>
+<li><a href="#">Career Profiling test</a></li>
+<li><a href="#">Abroad Education</a></li>
+<li><a href="#">Counselling</a></li>
+<li><a href="#">Counselling Video</a></li>
+<li><a href="#">Placement</a></li>
+</ul>
+</div>
+
+<div class="footerWidget">
+<h3>Connect with us</h3>
+<ul>
+<li><a href="#">Login | Register</a></li>
+<li><a href="college-login1.html">College Login</a></li>
+<li><a href="#">Chat</a></li>
+<li><a href="#">Blog</a></li>
+<li><a href="#">News</a></li>
+<li><a href="#">Contact us</a></li>
+<li><a href="#">Write reviews</a></li>
+</ul>
+</div>
+
+<div class="footerWidget">
+<h3>Links</h3>
+<ul>
+<li><a href="#">Search step by step</a></li>
+<li><a href="#">Common Application Form</a></li>
+<li><a href="#">Value of redeem coupon</a></li>
+<li><a href="#">Mobile Apps</a></li>
+</ul>
+</div>
+
+
+</section>
+
+<section class="footerBottom">
+<p>Copyright © 2017 mentorcell.com. All Rights Reserved</p>
+<div class="footerSocial">
+<a href="#" class="fbBg"><i class="icon-facebook"></i></a>
+<a href="#" class="twBg"><i class="icon-twitter"></i></a>
+<a href="#" class="inBg"><i class="icon-linkedin"></i></a>
+</div>
+</section>
+</div>
+</footer>
+
+
+<div class="clearfix"></div>
+</div>
+
+
+
+<!-- Login Modal Start -->
+<div id="loginModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Login</h4>
+      </div>
+      <div class="modal-body">
+      <div class="loginForm">
+      	<h3>Want to be a part of it? <a href="#" data-dismiss="modal" data-toggle="modal" data-target="#registerModal">Register here</a></h3>
+      	<form>
+      	<div class="inputRow">
+      		<input type="email" placeholder="Email Address" name="">
+      		<i class="icon-email"></i>
+      	</div>
+      	<div class="inputRow">
+
+      		<input type="password" placeholder="Password" name="">
+      		<i class="icon-key"></i>
+      	</div>
+
+      	<div class="inputRow">
+      		<input type="submit" value="Submit" class="go" name="">
+      	</div>
+
+      	<h4><input type="checkbox" name=""> Keep me signed in.</h4>
+      	<h5><a href="#">Forgot password?</a></h5>
+
+      	</form>
+
+      </div>
+      <div class="clearfix"></div>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- Login Modal Close -->
+
+
+
+<!-- Register Modal Start -->
+<div id="registerModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+    <div class="regRight">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Register for a new account</h4>
+      </div>
+      <div class="modal-body">
+      <div class="regForm">
+      	<h3>Already have an account? <a href="#"  data-dismiss="modal" data-toggle="modal" data-target="#loginModal">Login here</a></h3>
+      	<form>
+      	<div class="inputRow">
+      		<input type="email" placeholder="Email Address" name="">
+      		<i class="icon-email"></i>
+      	</div>
+      	<div class="inputRow">
+      		<input type="password" placeholder="Password" name="">
+      		<i class="icon-key"></i>
+      	</div>
+      	<div class="inputRow">
+      		<input type="text" placeholder="Mobile Number" name="">
+      		<i class="icon-phone-call"></i>
+      	</div>
+
+      	<div class="inputRow">
+      		<select><option>Education Interests</option></select>
+      		<i class="icon-education"></i>
+      	</div>
+
+      	<div class="inputRow">
+      		<select><option>Desired Courses</option></select>
+      		<i class="icon-course"></i>
+      	</div>
+
+      	<div class="inputRow">
+      		<select><option>Current City</option></select>
+      		<i class="icon-city"></i>
+      	</div>
+
+      	<div class="inputRow">
+      		<input type="submit" value="Submit" class="go" name="">
+      	</div>
+
+      	</form>
+
+      </div>
+      <div class="clearfix"></div>
+      </div>
+      </div>
+
+
+    </div>
+
+  </div>
+</div>
+<!-- Register Modal Close -->
+
+
+
+<!-- team 1 start -->
+<div id="teamModal1" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+    <div class="regRight">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Amit Kumar Gupta</h4>
+      </div>
+      <div class="modal-body">
+      <div class="teamPopup">
+      <div class="teamImg"><img src="images/ak-gupta.jpg"></div>
+      <h5>Co-Founder</h5>
+      <p>Amit Kumar Gupta did his Bachelors and Masters in Mechanical Engineering from IIT Kharagpur and holds a laurea Magistrale in Industrial Engineering & Management from University of Udine, Italy.  He has more than ten years of experience in Manufacturing Industry in the field of Supply Chain, in India and Italy. During his job tenure, he has been continuously guiding, training and developing teams working under him. He has a deep interest in Mentoring and high belief in moral education.</p>
+      </div>
+      <div class="clearfix"></div>
+      </div>
+      </div>
+
+
+    </div>
+
+  </div>
+</div>
+<!-- team 1 Close -->
+
+<!-- team 2 start -->
+<div id="teamModal2" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+    <div class="regRight">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Sanjeev Singh</h4>
+      </div>
+      <div class="modal-body">
+      <div class="teamPopup">
+      <div class="teamImg"><img src="images/sanjeev-singh.jpg"></div>
+      <h5>Co-Founder</h5>
+      <p>Sanjeev Singh holds a Masters in Computer Science from USC, Columbia, USA and Bachelors & Masters in Maths and Computing from IIT Kharagpur. He has over 10 years of experience in industry and academia. He has worked as a web and app developer, and was involved in research in mobile computing, particularly in smartphone assisted safe driving.</p>
+      </div>
+      <div class="clearfix"></div>
+      </div>
+      </div>
+
+
+    </div>
+
+  </div>
+</div>
+<!-- team 2 Close -->
+
+
+<!-- team 3 start -->
+<div id="teamModal3" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+    <div class="regRight">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Pranab Chaudhuri</h4>
+      </div>
+      <div class="modal-body">
+      <div class="teamPopup">
+      <div class="teamImg"><img src="images/p-chaudhuri.jpg"></div>
+      <h5>Co-Founder</h5>
+      <p>Pranab Chaudhuri holds a Bachelors and Masters in Maths & Computing from IIT Kharagpur. He has around 11 years of experience in industry working with High Tech companies like Oracle and VMware. He underwent training in 3D immersive visualization at NASA John C Stennis Space Center, Mississippi. He has worked in various domains like cloud, SAAS, Analystics etc.</p>
+      </div>
+      <div class="clearfix"></div>
+      </div>
+      </div>
+
+
+    </div>
+
+  </div>
+</div>
+<!-- team 3 Close -->
+
+<!-- team 4 start -->
+<div id="teamModal4" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+    <div class="regRight">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Dinesh Mishra</h4>
+      </div>
+      <div class="modal-body">
+      <div class="teamPopup">
+      <div class="teamImg"><img src="images/d-mishra.jpg"></div>
+      <h5>Co-Founder</h5>
+      <p>Dinesh Mishra did his Graduation in Hotel Management and MBA in Hospitality Management. He has almost 13 years of experience in Hospitality industry in India and abroad. He also has more than ten years of experience in educational consultancy sector and founded Edu Mantra Educational Society in the year 2009.</p>
+      </div>
+      <div class="clearfix"></div>
+      </div>
+      </div>
+
+
+    </div>
+
+  </div>
+</div>
+<!-- team 4 Close -->

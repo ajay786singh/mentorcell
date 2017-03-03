d0b2c136d2e9e05eac90fbbc51e3b975405b50d3
manishgoyal90 <manishgoyal.90@gmail.com>
2017-03-03 14:56:08 +0530

    exam issue fix

--- application/views/admin/_templates/footer.php
+++ application/views/admin/_templates/footer.php
@@ -187,5 +187,64 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	});	
 	/*otp verification*/
 </script>
+
+
+<script src="//cloud.tinymce.com/stable/tinymce.min.js"></script>
+  <script>tinymce.init({
+  selector: 'textarea',
+  height: 500,
+  theme: 'modern',
+  plugins: [
+    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+    'searchreplace wordcount visualblocks visualchars code fullscreen',
+    'insertdatetime media nonbreaking save table contextmenu directionality',
+    'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc'
+  ],
+  toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+  toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
+  image_advtab: true,
+  templates: [
+    { title: 'Test template 1', content: 'Test 1' },
+    { title: 'Test template 2', content: 'Test 2' }
+  ],
+  content_css: [
+    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+    '//www.tinymce.com/css/codepen.min.css'
+  ]
+ });</script>
+ 
+   <!-- /.content-wrapper -->
+ <script type="text/javascript" language="javascript">
+
+		var field_sr = 1;
+	function important_dates(){
+			field_sr++;
+			var numItems = jQuery('.important_dates_class').length;
+			if(numItems <10){
+			var field_html = '<p id="important_dates_p_'+field_sr+'"  class="important_dates_class"><input type="text"  id="datepicker" name="important_dates[]"  placeholder="date"  style="width:30%"> <input type="text" name="important_dates_description[]" placeholder="Content" style="width:50%" /> <a onclick = "return delete_field('+field_sr+');"  style="cursor:pointer">Delete</a></p>';
+			$("#important_dates_field_reapeater").append(field_html);
+			$("#important_dates_limit_msg").html("");
+			}else{
+				$("#important_dates_limit_msg").html("Max 9 important dates allowed.");
+			}
+		}
+		
+	function delete_field(pid){
+		$("#important_dates_p_"+pid).remove();
+		$("#important_dates_limit_msg").html("");
+	}
+
+ $('#datepicker').datepicker({
+      autoclose: true
+    });
+	
+	function create_slug(exam_name){
+		var slug_name  = exam_name.replace(" ","_");
+		var final_slug_name = slug_name.toLowerCase();
+		$("#slug").val(final_slug_name);
+	}
+
+</script>
+
     </body>
 </html>

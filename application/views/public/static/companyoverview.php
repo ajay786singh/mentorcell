0bbaaef0262a65957d75fb283a023ecb8afab555
Google123@# <ashutosh@zombiesoftwares.com>
2017-03-04 15:55:27 +0530

    added blog and static files

---
+++ application/views/public/static/companyoverview.php
@@ -0,0 +1,23 @@
+<section class="bgWhite">
+<div class="container">
+<div class="row">
+<div class="containerBox">
+<div class="sectionHeading">Company Overview</div>
+
+<div class="content">
+</div>
+
+
+
+
+
+
+
+</div>
+</div>
+</div>
+</section>
+
+
+
+
